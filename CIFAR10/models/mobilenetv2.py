41f322568ced3d471567288783c2b545a1762b28
Rozi1 <rozi.kh2@gmail.com>
2020-11-21 11:09:29 +0500

    Update mobilenetv2.py

--- CIFAR10/models/mobilenetv2.py
+++ CIFAR10/models/mobilenetv2.py
@@ -54,7 +54,7 @@ class MobileNetV2(nn.Module):
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
         self.layers = self._make_layers(in_planes=32)
-        self.conv2 = QuantConv2d(320, 1280, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv2 = nn.Conv2d(320, 1280, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn2 = nn.BatchNorm2d(1280)
         self.linear = nn.Linear(1280, num_classes)
 
